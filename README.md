54bace7d4e9d854a48476c8d0779a7513b59825a
Colin-Paslay <148103002+Colin-Paslay@users.noreply.github.com>
2023-12-01 10:52:29 -0600

    Multi-Step Problems (OfO doesn't work yet)
    You can now do 1 + 1 + 1, unfortunately I haven't figured out how to make it follow the order of operations yet.

--- README.md
+++ README.md
@@ -4,11 +4,6 @@ Can add/subtract/multiply/divide
 
 Has a decimal and negative number function
 
-NOTE: You can not do two operations at the same time
-
-1 + 4 * 5 = 5 and not 21
-
-1 / 3 * 3 = 0.3334 and not 3
-
-You will have to do 4 * 5 then 20 + 1
-or 1 / 3 then 0.33 * 3 instead of both at once
+Note UPDATE: You can do multible problems but it won't follow the order
+of operations yet.
+ex: 1 + 2 + 3 = 6 works fine but 3 - 3 * 3 = 0 and not -6
--- calculator.gd
+++ calculator.gd
@@ -8,20 +8,33 @@ func _ready():
 func _number_buttons(button):
 	NumBox.text += button.name
 func _on_equal_pressed():
-	var result: float
 	var agggg = NumBox.text.split(" ", false, 0)
-	var number1: float = agggg[0].to_float()
-	var operator = agggg[1]
-	var number2: float = agggg[2].to_float()
-	match operator:
-		"+":
-			result = number1 + number2
-		"-":
-			result = number1 - number2
-		"/":
-			result = number1 / number2
-		"*":
-			result = number1 * number2
+	var result: float = agggg[0].to_float()
+	var count: float = agggg.size() - 1
+	if count <= 1:
+		pass
+	else:
+		var finder = 1
+		var number1 = agggg[0].to_float()
+		var number2 = agggg[finder+1].to_float()
+		var operator = agggg[finder]
+		while count > 0:
+			match operator:
+				"+":
+					result = number1 + number2
+				"-":
+					result = number1 - number2
+				"/":
+					result = number1 / number2
+				"*":
+					result = number1 * number2
+			count -= 2
+			if count <= 1:
+				break
+			finder += 2
+			number1 = result
+			operator = agggg[finder]
+			number2 = agggg[finder+1].to_float()
 	NumBox.text = str(result)
 func _on_add_pressed():
 	NumBox.text += " + "
